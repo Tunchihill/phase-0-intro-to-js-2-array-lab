912f2f893af507d356edc3c2fb1a2c11b6cd8568
Arnold Kiprono Langat <arnoldkiprono3@gmail.com>
2024-04-03 04:29:15 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    name.push(name)
+    return cats
+
+}
+
+
+
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+    return cats
+}
+
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+    return cats
+}
+
+
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+    return cats
+}
+
+
+
+
+
+function appendCat(name) {
+    const newCats = cats.slice();
+    newCats.push(name); 
+    return newCats; 
+}
+
+
+
+
+
+function prependCat(name) {
+    const newCats = [name, ...cats];
+    return newCats; 
+}
+
+
+
+
+function removeLastCat(){
+    cats.pop()
+    return cats
+}
+
+
+function removeFirstCat() {
+    cats.shift(); 
+    return cats; 
+}
+
